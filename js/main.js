3f6a3ae7c002913de6b94bc547f3160834e153fa
nikolai <nikolai@sagewing.com>
2014-02-05 17:06:10 +0300

    Fix bugs. Remove repeated part in code Add template (jsRender) Data removed to json file

--- js/main.js
+++ js/main.js
@@ -1,16 +1,59 @@
 var GTD = GTD || {};
 
-GTD.taskStatus = {
-    NOT_STARTED: 1,
-    IN_PROGRESS: 2,
-    DONE: 3
+GTD.makeDraggable = function() {
+
+    _.each($('.issue'), function(issue) {
+        var isNew = $(issue).attr('id') == 'add-new-task';
+        //console.log($(issue), 'made draggable');
+        $(issue).draggable({
+            stop: function( event, ui ) {
+                _.each($('.progress-col'), function(column) {
+                    var $column = $(column);
+                    if (($column.offset().left <= ui.offset.left) && ($column.offset().left + $column.width() >= ui.offset.left) &&
+                        ($column.offset().top <= ui.offset.top) && ($column.offset().top + $column.height() >= ui.offset.top)) {
+
+                        if (isNew) {
+                            // get new id
+                            var dragElementId = 1 + _.max(GTD.tasks.data, function(task) {
+                                    return task.id;
+                                }).id,
+                                newStatus = GTD.getStatus(column.id),
+                                newType = $('#issue-type').val();
+
+                            GTD.tasks.add({id: dragElementId, name: 'New Issue', type: newType, status: newStatus})
+                        } else {
+                            var dragElementId = $(event.target).children('.issue-id').text(),
+                                newStatus = GTD.getStatus(column.id);
+
+                            GTD.tasks.edit(dragElementId, {status: newStatus});
+                            //GTD.tasks.draw();
+                        }
+
+                    }  else {
+                        $(event.target).css({left: 0, top: 0});
+                    }
+                });
+            }
+        });
+    });
 };
 
-GTD.taskType = {
-    TASK: 1,
-    BUG: 2
-};
 
+GTD.getStatus = function(id) {
+    var status = '';
+    switch (id) {
+        case 'not-started':
+             status = GTD.taskStatus.NOT_STARTED;
+            break;
+        case 'in-progress':
+            status = GTD.taskStatus.IN_PROGRESS;
+            break;
+        case 'finished':
+            status = GTD.taskStatus.DONE;
+            break;
+    }
+    return status;
+};
 
 GTD.localStorageDataName = 'issues-data';
 
@@ -22,7 +65,7 @@ GTD.tasks = {
         newTask.status = newTask.status || GTD.taskType.NOT_STARTED;
         self.data.push(newTask);
         self.draw();
-        self.saveData();
+        GTD.saveData();
     },
     edit: function(id, newData) {
         var self = this;
@@ -41,7 +84,7 @@ GTD.tasks = {
             }
         });
         self.draw();
-        self.saveData();
+        GTD.saveData();
 
     },
     remove: function(id) {
@@ -52,95 +95,80 @@ GTD.tasks = {
             }
         });
         self.draw();
-        self.saveData();
+        GTD.saveData();
     },
     draw: function() {
         var self = this,
-            $parent = $(this.selector),
-            $not_started, $inprogress, $finished; // 3 columns to output task/bugs
+            $parent = $(this.selector);
 
-        $parent.html('');
-        $not_started = $('<div id="not-started" class="col-md-4 progress-col"><h2>Not started</h2></div>');
-        $parent.append($not_started)
-        $inprogress = $('<div id="in-progress" class="col-md-4 progress-col"><h2>In progress</h2></div>');
-        $parent.append($inprogress)
-        $finished = $('<div id="finished" class="col-md-4 progress-col"><h2>Done</h2></div>');
-        $parent.append($finished);
+        $parent.html(
+            $('#statusColumns').render(GTD.columnsData)
+        );
 
         _.each(self.data, function(task) {
-            var issueTypeClassName = (task.type == 1) ? 'task' : 'bug',
-                $task = $('<div class="issue jumbotron ' + issueTypeClassName+'">' +
-                            '<div class="close">X</div>' +
-                            '<span class="issue-id">' + task.id + ' </span>'  +
-                            '<span class="issue-name"> ' + task.name + ' </span>' +
-                            '<input class="issue-edit" type="text" placeholder="' + task.name + '" value="' + task.name+ '" />' +
-                            '<button class="issue-save btn btn-primary">Save</button> ' +
-                        '</div>');
+            var renderData = {};
 
-            $task.draggable({
-                stop: function( event, ui ) {
-                    //console.log(event, ui);
-                    _.each($('.progress-col'), function(column) {
-                        var $column = $(column);
-                        if (($column.offset().left <= ui.offset.left) && ($column.offset().left + $column.width() >= ui.offset.left) &&
-                            ($column.offset().top <= ui.offset.top) && ($column.offset().top + $column.height() >= ui.offset.top)) {
-
-                            var dragElementId = $(event.target).children('.issue-id').text();
-                            var newStatus;
-
-                            switch (column.id) {
-                                case 'not-started':
-                                        newStatus = GTD.taskStatus.NOT_STARTED;
-                                    break;
-                                case 'in-progress':
-                                    newStatus = GTD.taskStatus.IN_PROGRESS;
-                                    break;
-                                case 'finished':
-                                    newStatus = GTD.taskStatus.DONE;
-                                    break;
-                            }
-                            self.edit(dragElementId, {status: newStatus});
-                            self.draw();
-
-                        }  else {
-                            $(event.target).css({left: 0, top: 0});
-                        }
-                    });
-                }
-            });
+            renderData.issueTypeClassName = (task.type == 1) ? 'task' : 'bug';
+            renderData.task = task;
 
 
-            switch(task.status) {
-                case GTD.taskStatus.NOT_STARTED:
-                        $not_started.append($task);
-                    break;
-                case GTD.taskStatus.IN_PROGRESS:
-                        $inprogress.append($task);
-                    break;
-                case GTD.taskStatus.DONE:
-                        $finished.append($task);
-                    break;
-                default:
-                        $not_started.append($task);
-                    break;
-            }
+            var columnHtmlId = _.find(GTD.columnsData, function(column) {
+                return column.id == task.status;
+            }).htmlId;
+
+            $('#' + columnHtmlId).append(
+                $('#taskTmlp').render(renderData)
+            );
+
         });
-    },
-    saveData: function() {
-        localStorage.setItem(GTD.localStorageDataName, JSON.stringify(this.data));
-    },
-    loadData: function() {
-        var data = localStorage.getItem(GTD.localStorageDataName);
-        if (data) {
-            this.data = JSON.parse(data);
-        } else {
-            this.data = GTD.tasksList;
-        }
-        GTD.tasks.draw();
+
+        GTD.makeDraggable();
     }
 };
 
+
+GTD.saveData = function() {
+    var data = {
+        tasks: this.tasks.data,
+        columnsData: this.columnsData
+    };
+    localStorage.setItem(GTD.localStorageDataName, JSON.stringify(data));
+};
+
+GTD.loadData = function() {
+    var data = localStorage.getItem(this.localStorageDataName),
+        self = this;
+    if (data) {
+        data = JSON.parse(data);
+        self.tasks.data = data.tasks;
+        self.columnsData = data.columnsData;
+        self.taskStatus = data.taskStatus;
+        self.taskType = data.taskType;
+        GTD.tasks.draw();
+    } else {
+        console.log('ajax');
+        $.ajax(
+            {
+                url: "js/data.json",
+                success: function(data) {
+                    self.tasks.data = data.taskList;
+                    self.columnsData = data.columns;
+                    self.taskStatus = data.taskStatus;
+                    self.taskType = data.taskType;
+                    GTD.tasks.draw();
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log(jqXHR, textStatus, errorThrown);
+                }
+            }
+        );
+    }
+
+};
+
 $(document).ready(function() {
+
+
 
     // remove issue
     $('#issues-panel').on('click', '.close', function() {
@@ -162,43 +190,9 @@ $(document).ready(function() {
     });
 
 
-    $('#add-new-task').draggable({
-        stop: function( event, ui ) {
-            _.each($('.progress-col'), function(column) {
-                var $column = $(column);
-                if (($column.offset().left <= ui.offset.left) && ($column.offset().left + $column.width() >= ui.offset.left) &&
-                    ($column.offset().top <= ui.offset.top) && ($column.offset().top + $column.height() >= ui.offset.top)) {
-
-                    // get new id
-                    var dragElementId = 1 + _.max(GTD.tasks.data, function(task) {
-                        return task.id;
-                    }).id;
-                    var newStatus, newType;
-
-                    switch (column.id) {
-                        case 'not-started':
-                            newStatus = GTD.taskStatus.NOT_STARTED;
-                            break;
-                        case 'in-progress':
-                            newStatus = GTD.taskStatus.IN_PROGRESS;
-                            break;
-                        case 'finished':
-                            newStatus = GTD.taskStatus.DONE;
-                            break;
-                    }
-
-                    newType = $('#issue-type').val();
-                    GTD.tasks.add({id: dragElementId, name: 'New Issue', type: newType, status: newStatus})
-
-                }  else {
-                    $(event.target).css({left: 0, top: 0});
-                }
-            });
-        }
-    });
-
+    //GTD.makeDraggable();
     GTD.tasks.selector = '#issues-panel';
-    GTD.tasks.loadData();
 
+    GTD.loadData();
 
 });
